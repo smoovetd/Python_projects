53c78502d35fb96ba0996828686c6d2c63a1776f
smoovetd <smoovetd@gmail.com>
2020-09-15 01:18:59 +0300

    Updated find_word.py to have case insensitive behavior - checks with lower, titled and upper version of entered word is it matched in the json dictionary

--- First_app_english_Thesaurus/find_word.py
+++ First_app_english_Thesaurus/find_word.py
@@ -3,21 +3,30 @@ import os
 
 g_words_file = 'files/data.json'
 
+#def find_similar_word(word:str) -> str:
+
+
 def find_word(word:str) -> str:
     '''find_word - takes argument word - and will be returned description from g_words_file content - default ..files/data.json '''
 
+    word_description = ''
     if os.path.exists(g_words_file) == False:
-        return 'ERROR no such file: ' + g_words_file
+        word_description = 'ERROR no such file: ' + g_words_file
     else:
-        word_description = ''
+
         with open(g_words_file, 'r') as data_file:
             file_content = json.load(data_file)
             if word in file_content.keys():
                 word_description = file_content[word]
-            elif: word.title() in file_content.keys():
-                return word_description
+            elif word.title() in file_content.keys():
+                word_description = file_content[word.title()]
+            elif word.lower() in file_content.keys():
+                word_description = file_content[word.lower()]
+            elif word.upper() in file_content.keys():
+                word_description = file_content[word.upper()]
             else:
-                return('Word: "' + word + '" is not found in dictionary! Please double check it.')
+                word_description = 'Word: "' + word + '" is not found in dictionary! Please double check it.'
+
         return word_description
 
 print(find_word(input('Enter word: ')))
