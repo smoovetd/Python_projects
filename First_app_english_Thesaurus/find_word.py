4e6fe69e9d79fd8bc3f3487ed5ae99d9c4a57c57
smoovetd <smoovetd@gmail.com>
2020-09-16 00:25:16 +0300

    Added option to search for similar word in dictionary in case of typo. Comparing is made based on matching letters over 75% from the length of the word and the first word is provided.

--- First_app_english_Thesaurus/find_word.py
+++ First_app_english_Thesaurus/find_word.py
@@ -2,8 +2,42 @@ import json
 import os
 
 g_words_file = 'files/data.json'
+g_matching_criteria_perc = 75
 
-#def find_similar_word(word:str) -> str:
+
+def get_min_length(first:str, second:str):
+        min_length = 0
+        if len(first) <= len(second):
+            min_length = len(first)
+        else:
+            min_length = len(second)
+
+        return min_length
+
+
+def compare_words(first:str, second:str):
+    matching_points = 0
+
+    max_index = get_min_length(first, second)
+    for index in range(0, max_index):
+        if first[index] == second[index]:
+            matching_points = matching_points + 1
+
+    return float(matching_points/len(first)) * 100
+
+
+def find_similar_word(word:str, keys_words:list) -> list:
+    word_lenght = len(word)
+    similar_words = []
+    similar_words_by_chars = []
+
+    possible_matches = [ new_word for new_word in keys_words if abs(len(new_word) - word_lenght) <=2]
+
+    for new_word in possible_matches:
+        if compare_words(word, new_word) >= g_matching_criteria_perc:
+            similar_words.append(new_word)
+
+    return similar_words
 
 
 def find_word(word:str) -> str:
@@ -13,7 +47,6 @@ def find_word(word:str) -> str:
     if os.path.exists(g_words_file) == False:
         word_description = 'ERROR no such file: ' + g_words_file
     else:
-
         with open(g_words_file, 'r') as data_file:
             file_content = json.load(data_file)
             if word in file_content.keys():
@@ -25,7 +58,10 @@ def find_word(word:str) -> str:
             elif word.upper() in file_content.keys():
                 word_description = file_content[word.upper()]
             else:
+                possible_matches = find_similar_word(word, file_content.keys())
                 word_description = 'Word: "' + word + '" is not found in dictionary! Please double check it.'
+                if len(possible_matches) != 0 and  input('Did you mean word: ' + possible_matches[0] + '? Y for yes, any key for no: ') == 'Y':
+                    word_description = file_content[possible_matches[0]]
 
         return word_description
 
