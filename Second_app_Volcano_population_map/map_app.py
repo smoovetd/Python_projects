8bedb28d3957a267ea9795e8bd7e2e6c4ba26ca0
smoovetd <smoovetd@gmail.com>
2020-10-07 01:06:21 +0300

    Updated popup information - to be html text. Updated color of the markers depending on height.

--- Second_app_Volcano_population_map/map_app.py
+++ Second_app_Volcano_population_map/map_app.py
@@ -2,6 +2,22 @@ import folium
 import webbrowser
 import pandas
 
+def get_color_by_elevation(elevation: float) -> str:
+    result = '';
+
+    if elevation < 2000:
+        result = 'green'
+    elif elevation < 3000:
+        result = 'orange'
+    else:
+        result = 'red'
+
+    return result
+
+def get_elements_pandas(file_path: str) -> list:
+
+    return []
+
 def get_elements(file_path:str ) -> list:
     content = []
     result = []
@@ -29,8 +45,8 @@ def get_elements(file_path:str ) -> list:
     #print(result)
     return result
 
-startup_coordinates = [44.67,23.37]
-def_zoom = 14
+startup_coordinates = [48.67,-123.37]
+def_zoom = 5
 file_name = 'files/map1.html'
 
 crnt_map = folium.Map(location=startup_coordinates, zoom_start = def_zoom, tiles='Stamen Terrain')
@@ -45,15 +61,19 @@ crnt_map = folium.Map(location=startup_coordinates, zoom_start = def_zoom, tiles
 fg = folium.FeatureGroup(name = 'Volcanoes')
 #fg.add_child(home_point)
 all_volcanoes = get_elements('files/Volcanoes.txt')
-
+html_popuptext = """<h4>Volcanoe Information:<h4>
+name: %s<br>location: %s<br>height: %s m
+"""
 for volcanoe in all_volcanoes:
     lat = volcanoe['latitude']
     lon = volcanoe['lontitude']
+    iframe = folium.IFrame(html = html_popuptext % (volcanoe['name'], volcanoe['location'], volcanoe['height']), width=250, height=100)
+
     crnt_marker = folium.Marker(location=[lat, lon ],
                                      tooltip = volcanoe['name'],
-                                     popup = 'name: %s\nlocation: %s\nheight: %s' % (volcanoe['name'], volcanoe['location'], int(volcanoe['height'])),
-                                     icon=folium.Icon(color='red')
-                                     )
+                                     popup =  folium.Popup(iframe),
+                                     icon=folium.Icon(color=get_color_by_elevation(volcanoe['height'])
+                                     ))
     fg.add_child(crnt_marker)
 
 crnt_map.add_child(fg)
