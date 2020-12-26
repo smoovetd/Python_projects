ab34ae6a66e48fc1f75a42a67471f57add7d5aee
smoovetd <smoovetd@gmail.com>
2020-12-27 00:51:54 +0200

    Added SignUpScreenSuccess to print message for successful sign up

--- Fifth_app_feel_good_mob_app/main.py
+++ Fifth_app_feel_good_mob_app/main.py
@@ -20,21 +20,23 @@ class SignUpScreen(Screen):
     def add_user(self, username, password):
         users = dict()
         if is_file_empty('users.json') == True:
-            print('SignUpScreen in here')
+            #print('SignUpScreen in here')
             with open('users.json', 'r') as file:
                 users = json.load(file)
-
 
         users[username] = {
             'username': username,
             'password': password,
             'created' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
 
-        print(users)
-
+        #print(users)
         with open('users.json', 'w') as file:
             json.dump(users, file)
 
+        self.manager.current = 'sign_up_screen_success'
+
+class SignUpScreenSuccess(Screen):
+    pass
 
 class RootWidget(ScreenManager):
     pass
