7b35c4335d19559fef1ee6e9cc4a6d4df263eea2
smoovetd <smoovetd@gmail.com>
2020-12-26 23:44:27 +0200

    Added Sign Up page and writing user and password to users.json file

--- Fifth_app_feel_good_mob_app/main.py
+++ Fifth_app_feel_good_mob_app/main.py
@@ -1,8 +1,16 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
+import os
 
 Builder.load_file('design.kv')
+
+def is_file_empty(path):
+    is_empty = os.path.isfile(path) and os.path.getsize(path) > 0
+    print(path, is_empty)
+    return is_empty
 
 class LoginScreen(Screen):
     def sign_up(self):
@@ -10,7 +18,23 @@ class LoginScreen(Screen):
 
 class SignUpScreen(Screen):
     def add_user(self, username, password):
-        print(username, password)
+        users = dict()
+        if is_file_empty('users.json') == True:
+            print('SignUpScreen in here')
+            with open('users.json', 'r') as file:
+                users = json.load(file)
+
+
+        users[username] = {
+            'username': username,
+            'password': password,
+            'created' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+
+        print(users)
+
+        with open('users.json', 'w') as file:
+            json.dump(users, file)
+
 
 class RootWidget(ScreenManager):
     pass
