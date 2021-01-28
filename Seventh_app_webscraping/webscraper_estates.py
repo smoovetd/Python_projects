50e569cf2700f201fbc5390008d904383750a0f2
smoovetd <smoovetd@gmail.com>
2021-01-29 01:43:01 +0200

    Taken price for each estate.

--- Seventh_app_webscraping/webscraper_estates.py
+++ Seventh_app_webscraping/webscraper_estates.py
@@ -6,6 +6,9 @@ import pandas
 
 base_url = 'http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/'
 page_ext = 't=0&s='
+html_ext = '.html'
+df = pandas.DataFrame()
+properties = []
 
 html_content = requests.get(base_url, headers = {'User-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64l rv:61.0) Gecko/20100101 Firefox/61.0'})
 
@@ -18,6 +21,25 @@ else:
     content = BeautifulSoup(html_content.text, 'html.parser')
 
 #print(content.prettify())
+#finding max pages s
 page_nums = content.find_all('a',{'class':'Page'})
-max_pages = page_nums[-1].text
+max_pages = int(page_nums[-1].text)
 print(max_pages)
+
+for crnt_page in range(0, max_pages, 1):
+    #print(crnt_page * 10)
+    #print(base_url + page_ext + str(crnt_page * 10))
+    crnt_page_html = requests.get(base_url + page_ext + str(crnt_page * 10) + html_ext, headers = {'User-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64l rv:61.0) Gecko/20100101 Firefox/61.0'})
+    crnt_page_content = BeautifulSoup(crnt_page_html.text, 'html.parser')
+#    print(crnt_content.prettify())
+
+    all_props = crnt_page_content.find_all('div',{'class':'propertyRow'})
+
+    for property in all_props:
+        crnt_prop = {}
+        #print(property.find('h4').text)
+        crnt_prop['Price'] = property.find('h4',{'class':'propPrice'}).text.strip()
+        properties.append(crnt_prop)
+
+print(properties)
+print(df)
